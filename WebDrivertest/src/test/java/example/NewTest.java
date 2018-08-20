61ce678d07f2d43b111b7ab87142bf65825b8fa4
shadab36 <35285943+shadab36@users.noreply.github.com>
2018-08-20 16:30:16 +0530

    Update NewTest.java

--- WebDrivertest/src/test/java/example/NewTest.java
+++ WebDrivertest/src/test/java/example/NewTest.java
@@ -7,11 +7,13 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-          public class NewTest {
+
+
+public class NewTest {		
 	    public WebDriver driver;	
-//	    public static final String USERNAME = "coludapp";
-//		public static final String ACCESS_KEY = "fbdcfc53-58bd-4046-ad89-909c3dbc7cbd";
-//		public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+	    public static final String USERNAME = "coludapp";
+		public static final String ACCESS_KEY = "fbdcfc53-58bd-4046-ad89-909c3dbc7cbd";
+		public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
 		@Test				
 		public void testEasy() {	
 			driver.get("http://sivvi.com/");  
@@ -19,15 +21,15 @@ import org.testng.annotations.Test;
 			Assert.assertTrue(title.contains("SIVVI.COM")); 		
 		}	
 		@BeforeTest
-		public void beforeTest() {	
+		public void beforeTest() throws MalformedURLException {	
 			//System.setProperty("webdriver.chrome.driver", "F:\\Driver of All\\chromedriver.exe");
 		   // driver = new ChromeDriver();  
 			DesiredCapabilities caps = DesiredCapabilities.chrome();
-//			caps.setCapability("platform", "Windows 10");
-//			caps.setCapability("version", "64");
+			caps.setCapability("platform", "Windows 10");
+			caps.setCapability("version", "64");
 			caps.setCapability("name", "My Desktop automation test");
 			// here is the sauce driver 
-		//	driver = new RemoteWebDriver(new URL(http://coludapp:fbdcfc53-58bd-4046-ad89-909c3dbc7cbd@ondemand.saucelabs.com:80/wd/hub), caps);
+			 driver = new RemoteWebDriver(new URL(URL), caps);
 			//
 		}		
 		@AfterTest
@@ -35,6 +37,9 @@ import org.testng.annotations.Test;
 			driver.close();			
 		}		
 }	
+
+
+
 
 
 
