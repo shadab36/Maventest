9294c2751d39cfba89b23b2e2db1773e75a36b7b
shadab36 <35285943+shadab36@users.noreply.github.com>
2018-08-20 17:56:03 +0530

    Update NewTest.java

--- WebDrivertest/src/test/java/example/NewTest.java
+++ WebDrivertest/src/test/java/example/NewTest.java
@@ -9,29 +9,41 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class NewTest {		
-    public WebDriver driver;	
-//	    public static final String USERNAME = "coludapp";
-//		public static final String ACCESS_KEY = "fbdcfc53-58bd-4046-ad89-909c3dbc7cbd";
-//		public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
-	@Test				
-	public void testEasy() {	
-		driver.get("http://sivvi.com/");  
-	    String title = driver.getTitle();				 
-		Assert.assertTrue(title.contains("SIVVI.COM")); 		
-	}	
-@BeforeTest
-	public void beforeTest()   {	
-		//System.setProperty("webdriver.chrome.driver", "F:\\Driver of All\\chromedriver.exe");
-	   driver = new ChromeDriver();  
-	
-
-	
-	}		
-	@AfterTest
-	public void afterTest() {
-		driver.close();			
-	}		
+	    public WebDriver driver;	
+	    public static final String USERNAME = "coludapp";
+		public static final String ACCESS_KEY = "fbdcfc53-58bd-4046-ad89-909c3dbc7cbd";
+		public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+		@Test				
+		public void testEasy() {	
+			driver.get("http://sivvi.com/");  
+		    String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("SIVVI.COM")); 		
+		}	
+	@BeforeTest
+		public void beforeTest()   {	
+			//System.setProperty("webdriver.chrome.driver", "F:\\Driver of All\\chromedriver.exe");
+		  //  driver = new ChromeDriver();  
+		
+		
+		try {
+			DesiredCapabilities caps =DesiredCapabilities.chrome();
+			caps.setCapability("platform", "Windows 10");
+			caps.setCapability("version", "64");
+			caps.setCapability("name", "My Desktop automation test");
+			// here is the sauce driver 
+			 driver = new RemoteWebDriver(new java.net.URL(URL), caps);
+		}catch(Exception e) {
+			
+		}
+//			//
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.close();			
+		}		
 }	
+
+
 
 
 
