6121c8a43243ade96fb0fd6011eb4948683c00b1
shadab36 <35285943+shadab36@users.noreply.github.com>
2018-08-21 15:26:40 +0530

    Update NewTest.java
    test

--- WebDrivertest/src/test/java/example/NewTest.java
+++ WebDrivertest/src/test/java/example/NewTest.java
@@ -2,18 +2,27 @@ package example;
 
 
 
+import java.net.MalformedURLException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class NewTest {		
-	    public WebDriver driver;	
-//	    public static final String USERNAME = "AyushDam";
-//		public static final String ACCESS_KEY = "f5cae068-fd7e-46b2-92c5-3b639ef8a721";
-//		public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+	    public WebDriver driver;
+	   
+		public static String browserName;
+		public static String platformVersion;
+		public static String platform;
+		public static String platformName;
+	    public static final String USERNAME = "AyushDam";
+		public static final String ACCESS_KEY = "f5cae068-fd7e-46b2-92c5-3b639ef8a721";
+		public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
 		@Test				
 		public void testEasy() {	
 			driver.get("http://sivvi.com/");  
@@ -21,31 +30,25 @@ public class NewTest {
 			Assert.assertTrue(title.contains("SIVVI.COM")); 		
 		}	
 	@BeforeTest
-		public void beforeTest()   {	
-			System.setProperty("webdriver.chrome.driver", "F:\\Driver of All\\chromedriver.exe");
-		   driver = new ChromeDriver();  
+		public void beforeTest() throws MalformedURLException   {	
+//			System.setProperty("webdriver.chrome.driver", "F:\\Driver of All\\chromedriver.exe");
+//		   driver = new ChromeDriver();  
+//		
 		
-	}	
 
-//			DesiredCapabilities caps =DesiredCapabilities.chrome();
-//			caps.setCapability("platform", "Windows 10");
-//			caps.setCapability("version", "64");
-//			caps.setCapability("name", "My Desktop automation test");
-//			// here is the sauce driver 
-//			 driver = new RemoteWebDriver(new java.net.URL(URL), caps);
-		
+	DesiredCapabilities capability = new DesiredCapabilities();
+	capability.setCapability("platform", platform);
+	capability.setBrowserName(browserName);
+	capability.setCapability("version", platformVersion);
+	capability.setCapability("name", "AutoBill automation tests");
+	
+			 driver = new RemoteWebDriver(new java.net.URL(URL), capability);
+	}		
 		@AfterTest
 		public void afterTest() {
 			driver.close();			
 		}		
 }	
-
-
-
-
-
-
-
 
 
 
